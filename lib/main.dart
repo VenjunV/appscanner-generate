37e53f996e6a557bf75b04b29b11c2230f02674e
VenjunV <vallentevernie12@gmail.com>
2023-04-21 15:50:44 +0800

    scanner and generate qr

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:appscanner/home.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.yellow,
+    ),
+    home:  const MyCustomWidget(),
+  )
+  );
+}
